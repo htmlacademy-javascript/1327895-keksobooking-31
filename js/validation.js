f93781ad4671a6326d878c5fff7852c68c9399d0
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2024-05-08 22:57:07 +0300

    Правда или действие

---
+++ js/validation.js
@@ -0,0 +1,7 @@
+const adFormTitle = document.querySelector('#title');
+const adFormPrice = document.querySelector('#price');
+const adFormRoomNumber = document.querySelector('#room_number');
+const adFormCapacity = document.querySelector('#capacity');
+const adFormHousingType = document.querySelector('#type');
+const adFormTimein = document.querySelector('#timein');
+const adFormTimeout = document.querySelector('#timeout');
